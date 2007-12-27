7e260959130d97e945c797f5121ed422ed5f7aac
yns000 <yns000@464243a3-3722-0410-a3eb-d1584488e12a>
2007-12-27 19:25:18 +0000

    101 General House Keeping

---
+++ src/org/owasp/jbrofuzz/fuzz/dir/DRequestIterator.java
@@ -0,0 +1,301 @@
+/**
+ * DRequestIterator.java 0.6
+ *
+ * Java Bro Fuzzer. A stateless network protocol fuzzer for penetration tests.
+ * It allows for the identification of certain classes of security bugs, by
+ * means of creating malformed data and having the network protocol in question
+ * consume the data.
+ *
+ * Copyright (C) 2007 subere (at) uncon org
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA  02110-1301, USA.
+ */
+package org.owasp.jbrofuzz.fuzz.dir;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.prefs.Preferences;
+
+import org.apache.commons.httpclient.DefaultHttpMethodRetryHandler;
+import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.HttpException;
+import org.apache.commons.httpclient.HttpStatus;
+import org.apache.commons.httpclient.URIException;
+import org.apache.commons.httpclient.contrib.ssl.EasySSLProtocolSocketFactory;
+import org.apache.commons.httpclient.methods.GetMethod;
+import org.apache.commons.httpclient.params.HttpConnectionParams;
+import org.apache.commons.httpclient.params.HttpMethodParams;
+import org.apache.commons.httpclient.protocol.DefaultProtocolSocketFactory;
+import org.apache.commons.httpclient.protocol.Protocol;
+import org.apache.commons.httpclient.util.URIUtil;
+import org.owasp.jbrofuzz.io.FileHandler;
+import org.owasp.jbrofuzz.ui.JBRFrame;
+import org.owasp.jbrofuzz.version.JBRFormat;
+
+/**
+ * <p>
+ * Class for generating the recursive directory requests.
+ * </p>
+ * <p>
+ * Once the object is instantiated, the run method should be used to run through
+ * the requests.
+ * </p>
+ * 
+ * @author subere (at) uncon (dot) org
+ * @version 0.6
+ */
+public class DRequestIterator {
+
+	// The frame window that the request iterator
+	private JBRFrame m;
+	// The original url string
+	private String url;
+	// The vector of directories to be passed
+	private String[] directories;
+	// The vector of responses that will come back
+	private String[] responses;
+	// The boolean to see if the iterator has paused
+	private boolean stopped;
+	// The integer of the current request count
+	private int i;
+	// The port on which directory enumeration is taking place
+	private int port;
+
+	/**
+	 * <p>
+	 * Constructor for creating a web directory request iterator that iterates
+	 * through the directory listing.
+	 * </p>
+	 * 
+	 * @param m
+	 *          FrameWindow
+	 * @param url
+	 *          String
+	 * @param directories
+	 *          String
+	 * @param port
+	 *          int
+	 */
+	public DRequestIterator(final JBRFrame m, final String url,
+			final String directories, final String port) {
+		this.m = m;
+		this.url = url;
+		this.port = 0;
+		this.directories = directories.split("\n");
+		responses = new String[directories.length()];
+		stopped = false;
+		i = 0;
+
+		// Check the port
+		try {
+			this.port = Integer.parseInt(port);
+		} catch (final NumberFormatException e1) {
+			this.port = 0;
+			m.log("Web Directories Panel: Specify a valid port: \"" + port + "\"");
+		}
+		if ((this.port < 1) || (this.port > 65535)) {
+			this.port = 0;
+			m.log("Web Directories Panel: Port has to be between [1 - 65535]");
+		}
+		// Establish the protocols, if the port is valid
+		if (this.port != 0) {
+
+			// For https, allow self-signed certificates
+			if (this.url.startsWith("https://")) {
+				final Protocol easyhttps = new Protocol("https",
+						new EasySSLProtocolSocketFactory(), this.port);
+				Protocol.registerProtocol("https", easyhttps);
+			}
+			// For http, just show affection
+			if (this.url.startsWith("http://")) {
+				final Protocol easyhttp = new Protocol("http",
+						new DefaultProtocolSocketFactory(), this.port);
+				Protocol.registerProtocol("http", easyhttp);
+			}
+		}
+	}
+
+	/**
+	 * Check to see if the current Request Iterator is stopped.
+	 * 
+	 * @return boolean
+	 */
+	public boolean isStopped() {
+		return stopped;
+	}
+
+	/**
+	 * Method used for running the request iterator once it has been initialised.
+	 */
+	public void run() {
+		// Check for a valid URL
+		if (url.equalsIgnoreCase("")) {
+			return;
+		}
+		if (url.contains(" ")) {
+			return;
+		}
+		if ((port < 1) || (port > 65536)) {
+			return;
+		}
+
+		for (i = 0; i < directories.length; i++) {
+			if (stopped) {
+				return;
+			}
+
+			String currentURI = "";
+			try {
+				currentURI = url + URIUtil.encodePath(directories[i]);
+			} catch (final URIException ex) {
+				currentURI = "";
+				m.log("Could not encode the URI: " + url
+						+ directories[i]);
+			}
+
+			// Checks...
+			if (currentURI.equalsIgnoreCase("")) {
+				return;
+			}
+			if ((port <= 0) || (port > 65535)) {
+				return;
+			}
+
+			final HttpClient client = new HttpClient();
+			client.getParams().setParameter(HttpConnectionParams.CONNECTION_TIMEOUT,
+					Integer.valueOf(10000));
+
+			final GetMethod method = new GetMethod(currentURI);
+			method.getParams().setParameter(HttpMethodParams.RETRY_HANDLER,
+					new DefaultHttpMethodRetryHandler(1, false));
+			method.setFollowRedirects(true);
+			method.setDoAuthentication(true);
+
+			try {
+				responses[i] = i + "\n";
+				responses[i] += currentURI + "\n";
+				int statusCode = 0;
+				//
+				statusCode = client.executeMethod(method);
+				//
+				responses[i] += statusCode + "\n";
+				responses[i] += method.getStatusText() + "\n";
+
+				if (statusCode == HttpStatus.SC_OK) {
+					final ByteArrayOutputStream baos = new ByteArrayOutputStream();
+					final InputStream in = method.getResponseBodyAsStream();
+
+					final byte[] buff = new byte[65535];
+					int got;
+					while ((got = in.read(buff)) > -1) {
+						baos.write(buff, 0, got);
+					}
+					final byte[] allbytes = baos.toByteArray();
+
+					String results = null;
+					try {
+						results = new String(allbytes, method.getResponseCharSet());
+					} catch (final UnsupportedEncodingException ex1) {
+						m.log("Web Directories: Unsupported Character Encoding");
+						results = "";
+					}
+
+					// Check for comments
+					if (results.contains("<!--")) {
+						responses[i] += "Yes\n";
+					} else {
+						responses[i] += "No\n";
+					}
+					// Check for scripts
+					if ((results.contains("<script")) || (results.contains("<SCRIPT"))) {
+						responses[i] += "Yes\n";
+					} else {
+						responses[i] += "No\n";
+					}
+				}
+				// If no ok response has come back just append comments and scripts
+				else {
+					responses[i] += "No\n";
+					responses[i] += "No\n";
+				}
+			} catch (final HttpException e) {
+				responses[i] = i + "\n";
+				responses[i] += currentURI + "\n";
+				responses[i] += "000" + "\n";
+				responses[i] += "Fatal protocol violation" + "\n";
+				responses[i] += " \n";
+				responses[i] += " \n";
+				// Bomb out...
+				final Preferences prefs = Preferences.userRoot().node("owasp/jbrofuzz");
+				boolean continueOnError = prefs.getBoolean(JBRFormat.PREF_FUZZ_DIR_ERR,
+						false);
+				if (!continueOnError) {
+					stop();
+				}
+			} catch (final IOException e) {
+				responses[i] = i + "\n";
+				responses[i] += currentURI + "\n";
+				responses[i] += "000" + "\n";
+				responses[i] += "Fatal transport error" + "\n";
+				responses[i] += " \n";
+				responses[i] += " \n";
+				// Bomb out...
+				final Preferences prefs = Preferences.userRoot().node("owasp/jbrofuzz");
+				boolean continueOnError = prefs.getBoolean(JBRFormat.PREF_FUZZ_DIR_ERR,
+						false);
+				if (!continueOnError) {
+					stop();
+				}
+			} finally {
+				method.releaseConnection();
+			}
+			// Add a row to the displaying table
+			m.getWebDirectoriesPanel().addRow(responses[i]);
+			// Create a String to be written to file
+			final StringBuffer outToFile = new StringBuffer();
+			
+			final Date currentTime = new Date();
+			final SimpleDateFormat dateTime = new SimpleDateFormat(
+					"dd.MM.yyyy HH:mm:ss", new Locale("en"));
+			outToFile.append(dateTime.format(currentTime));
+			
+			// String outToFile = Time.dateAndTime();
+			final String[] tempArray = responses[i].split("\n");
+			for (final String element : tempArray) {
+				outToFile.append("," + element);
+			}
+			// Write the file
+			FileHandler.writeWebDirFile(m.getWebDirectoriesPanel()
+					.getSessionNumber(), outToFile.toString());
+			// Update the progress bar
+			final double percentage = 100 * ((double) (i + 1))
+					/ (directories.length);
+			m.getWebDirectoriesPanel().setProgressBar((int) percentage);
+		}
+	}
+
+	/**
+	 * Stop the Request Iterator, if it currently running.
+	 */
+	public void stop() {
+		stopped = true;
+	}
+}
---
+++ src/org/owasp/jbrofuzz/pub/GRequestIterator.java
@@ -0,0 +1,122 @@
+/**
+ * GRequestIterator.java 0.6
+ *
+ * Java Bro Fuzzer. A stateless network protocol fuzzer for penetration tests.
+ * It allows for the identification of certain classes of security bugs, by
+ * means of creating malformed data and having the network protocol in question
+ * consume the data.
+ *
+ * Copyright (C) 2007 subere (at) uncon org
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA  02110-1301, USA.
+ */
+package org.owasp.jbrofuzz.pub;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.owasp.jbrofuzz.ui.JBRFrame;
+
+/**
+ * <p>
+ * Class for looking domain addresses by making google requests.
+ * </p>
+ * <p>
+ * This class makes a total of 5 requests to google using the domain passed to
+ * the constructor.
+ * </p>
+ * 
+ * @author subere (at) uncon (dot) org
+ * @version 0.6
+ */
+public class GRequestIterator {
+	// The output obtained
+	private StringBuffer output;
+
+	public GRequestIterator(final JBRFrame mFrameWindow, final String domain) {
+		output = new StringBuffer();
+		int counter = 1;
+		BufferedReader bin = null;
+
+		while (counter <= 5) {
+			try {
+				// URL request = new URL("http://www.google.com");
+				final String urlRequest = "http://www.google.com/search?q=%40" + domain
+						+ "&hl=en&lr=&ie=UTF-8&start=" + counter + "0&sa=N";
+				final URL request = new URL(urlRequest);
+				final URLConnection connection = request.openConnection();
+				connection.setRequestProperty("User-Agent",
+						"Mozilla/4.0 (compatible; MSIE 5.01; Windows NT 5.0)");
+				connection.connect();
+
+				bin = new BufferedReader(new InputStreamReader(connection
+						.getInputStream()));
+				String line = null;
+				while ((line = bin.readLine()) != null) {
+					final Pattern p = Pattern.compile("([\\w\\.\\-\\%\\']+)@[\\S]+");
+					final Matcher m = p.matcher(line);
+					while (m.find()) {
+						final String email = sanitise(m.group());
+						output.append(email + " \n");
+					}
+				}
+				bin.close();
+			} catch (final MalformedURLException e) {
+				mFrameWindow.log("Open Source Tab: A malformed URL exception occured");
+			} catch (final IOException e) {
+				mFrameWindow.log("Open Source Tab: An IO exception occured");
+			}
+			if (bin != null) {
+				try {
+					bin.close();
+				} catch (final IOException ex) {
+				}
+			}
+
+			mFrameWindow.getOpenSourcePanel()
+					.appendOutputText(output.toString());
+			mFrameWindow.getOpenSourcePanel().setProgressBar(counter);
+			counter++;
+		} // while loop
+	}
+
+	public String getOutput() {
+		return output.toString();
+	}
+
+	private String sanitise(String in) {
+		boolean tagFound = false;
+		while (!tagFound) {
+			tagFound = true;
+			final int str = in.indexOf('<');
+			if (str > 0) {
+				final int end = in.indexOf('>', str);
+				if (end > 0) {
+					in = in.substring(0, str) + in.substring(end + 1);
+					tagFound = false;
+				}
+			}
+		}
+		return in;
+	}
+
+}
