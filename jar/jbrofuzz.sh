1ec2aee24ed87e697cd0b5358d86fa02330fa034
yns000 <yns000@464243a3-3722-0410-a3eb-d1584488e12a>
2009-12-06 20:17:15 +0000

    JBroFuzz 1.8

--- jar/jbrofuzz.sh
+++ jar/jbrofuzz.sh
@@ -1,1 +1,15 @@
-java -Xmx512 -jar JBroFuzz.jar
+#!/bin/sh
+
+JAVA=`which java`
+# JAVA_PARAMS="-XX:+PrintGCDetails -Xms32m -Xmx512m"
+JAVA_PARAMS="-Xmx512m"
+
+# Launch JBroFuzz
+if [ -r ./JBroFuzz.jar ]; then
+    $JAVA $JAVA_PARAMS -jar ./JBroFuzz.jar
+    exit 0;
+fi
+
+echo "Unable to find JBroFuzz.jar file.  "
+echo -n $0
+echo " needs to be in the same directory as JBroFuzz.jar"
