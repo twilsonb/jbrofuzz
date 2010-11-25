86f8d0ee60402cca32c145236d8179d509389a70
daemonmidi <daemonmidi@464243a3-3722-0410-a3eb-d1584488e12a>
2010-11-25 19:08:20 +0000

    bugfixes for writing to couchdb

--- src/org/owasp/jbrofuzz/db/dto/SessionDTO.java
+++ src/org/owasp/jbrofuzz/db/dto/SessionDTO.java
@@ -70,4 +70,42 @@ public class SessionDTO{
 	public ResponseDTO[] getResponse(){
 		return this.response;
 	}
+	
+	public void showForDebug(){
+		System.out.println("-------------------------------------------------------");
+		System.out.println("JVersion: " + this.jVersion);
+		System.out.println("OS: " + this.os);
+		System.out.println("SessionId: " + this.sessionId);
+		System.out.println("TimeStamp: " + this.timestamp);
+		System.out.println("-------------------------------------------------------");
+		ConnectionDTO connection = this.getConnectionDTO();
+		System.out.println("ConnectionId: " + connection.getConnectionId());
+		System.out.println("SessionId: " + connection.getSessionId());
+		System.out.println("URL: " + connection.getUrlString());
+		System.out.println("-------------------------------------------------------");
+		ResponseDTO[] responses = this.getResponse();
+		for (int i = 0; i < responses.length; i++){
+			ResponseDTO response = this.getResponse()[i];
+			System.out.println("ConncetionId: " + response.getConnectionId());
+			System.out.println("ResponseBody: " + response.getResponseBody());
+			System.out.println("ResponseHeader: " + response.getResponseHeader());
+			System.out.println("ResponseId: " + response.getResponseId());
+			System.out.println("StatusCode: " + response.getStatusCode());
+			System.out.println("+++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+		}
+		System.out.println("-------------------------------------------------------");
+		MessageDTO[] messages = this.getMessage();
+		for (int i = 0; i < messages.length; i++){
+			MessageDTO message = this.getMessage()[i];
+			System.out.println("ConnectionId: " + message.getConnectionId());
+			System.out.println("Encodig: " + message.getEncoding());
+			System.out.println("End: " + message.getEnd());
+			System.out.println("Start: " + message.getStart());
+			System.out.println("MessageId: " +  message.getMessageId());
+			System.out.println("Payload: " + message.getPayload());
+			System.out.println("Textrequest: " + message.getTextRequest());
+			System.out.println("-------------------------------------------------------");
+		}
+		System.out.println("-------------------------------------------------------");
+	}
 }
